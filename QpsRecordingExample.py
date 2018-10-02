2c3c86b6fa3d21f0ff9b7186a4c480b9fc8cbd29
mholsey <mholsey@265bcc82-57df-11dd-8522-2d0c19f12d9e>
2018-10-02 13:04:17 +0000

    Updated Script to use up-to-date QuarchPy Methods.
    git-svn-id: https://quarchserver4.ds.quarch.com/svn/Software/Application%20Notes/AN-015%20-%20Automating%20with%20QPS/Trunk@2616 265bcc82-57df-11dd-8522-2d0c19f12d9e

--- QpsRecordingExample.py
+++ QpsRecordingExample.py
@@ -1,11 +1,12 @@
 '''
 AN-015 - Application note demonstrating automated control over Quarch Power Studio (QPS)
 
-This example demonstrates
+This example demonstrates adding annotations and datapoints to a QPS stream.
 
 ########### VERSION HISTORY ###########
 
 05/06/2018 - Andy Norrie	- First Version
+02/10/2018 - Matt Holsey    - Updated Script for updated QuarchPy Methods.
 
 ########### INSTRUCTIONS ###########
 
