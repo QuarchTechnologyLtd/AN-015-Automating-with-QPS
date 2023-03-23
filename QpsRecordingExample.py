b0ff532c7876b8d9459aed065e7e2b6a5e2cff9a
gseed <gseed@265bcc82-57df-11dd-8522-2d0c19f12d9e>
2023-03-23 10:50:55 +0000

    Updates further to general review of ANs.
    git-svn-id: https://quarchserver4.ds.quarch.com/svn/Software/Application%20Notes/AN-015%20-%20Automating%20with%20QPS/Trunk@6935 265bcc82-57df-11dd-8522-2d0c19f12d9e

--- QpsRecordingExample.py
+++ QpsRecordingExample.py
@@ -1,24 +1,38 @@
-'''
+"""
 AN-015 - Application note demonstrating automated control over Quarch Power Studio (QPS)
 
 This example demonstrates adding annotations and datapoints to a QPS stream.
 
 ########### VERSION HISTORY ###########
 
-05/06/2018 - Andy Norrie    - First Version
+05/06/2018 - Andy Norrie    - First Version.
 02/10/2018 - Matt Holsey    - Updated Script for updated QuarchPy Methods.
 15/10/2020 - Pedro Cruz     - Updated Script for PAM.
+23/03/2023 - Graham Seed    - Reviewed code and updated requirements and instructions.
+
+########### REQUIREMENTS ###########
+
+1- Python (3.x recommended)
+    https://www.python.org/downloads/
+2- Quarchpy python package
+    https://quarch.com/products/quarchpy-python-package/
+3- Quarch USB driver (Required for USB connected devices on Windows only)
+    https://quarch.com/downloads/driver/
+4- Check USB permissions if using Linux:
+    https://quarch.com/support/faqs/usb/
 
 ########### INSTRUCTIONS ###########
 
-For localhost QPS, run the example as it is.
-For remote QPS, comment out the 'startLocalQps()' command and specify the IP:Port in the qpsInterface(...) command
-This can also be used if you want to use a different version of QPS and will run it yourself
+1- Connect a Quarch module to your PC via USB, Serial or LAN and power it on.
+2- Run the script and follow the instructions on screen.
+3- For localhost QPS, run the example as it is.
+4- For remote QPS, comment out the 'startLocalQps()' command and specify the IP:Port in the qpsInterface(...) command
+   This can also be used if you want to use a different version of QPS and will run it yourself
 
 ####################################
-'''
+"""
 
-# OS allows us access to path data
+# Import other libraries used in the examples
 import os
 import time
 
@@ -28,55 +42,66 @@ from quarchpy import qpsInterface, isQpsRunning, startLocalQps, GetQpsModuleSele
 
 
 def main():
+    # If required you can enable python logging, quarchpy supports this and your log file
+    # will show the process of scanning devices and sending the commands.  Just comment out
+    # the line below.  This can be useful to send to quarch if you encounter errors
+    # logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+
+    print("\n\nQuarch application note example: AN-015")
+    print("---------------------------------------\n\n")
+
     # Version 2.0.15 or higher expected for this application note
-    requiredQuarchpyVersion ("2.0.15")
+    requiredQuarchpyVersion("2.0.15")
 
-
-    #File paths for the example are set here, and can be altered to put your data files in a different location
-    #The path must be writable via a standard OS path
+    # File paths for the example are set here, and can be altered to put your data files in a different location
+    # The path must be writable via a standard OS path
     filePath = os.path.dirname(os.path.realpath(__file__))
 
     # Checks if QPS is running on the localhost
-    if isQpsRunning() == False:
+    if isQpsRunning() is False:
         # Start QPS from quarchpy
         startLocalQps()
 
-    # Connect to the localhost QPS instance - you can also specify host='127.0.0.1' and port=*************************************??????? for remote control.
-    # This is used to access the basic functions, allowing us to scan for devices.  This step can be skipped if you already know the ID
-    # string of the device you want to connect to
+    # Connect to the localhost QPS instance - you can also specify host='127.0.0.1' and port=xxxx for remote control.
+    # This is used to access the basic functions, allowing us to scan for devices.
     myQps = qpsInterface()
 
-    #Display and choose module from found modules. This returns a String with the connectionTarget to the device. USB::QTL1999-05-005 or TCP::192.168.1.1
+    # Display and choose module from found modules.
+    # This returns a String with the connectionTarget to the device; eg USB::QTL1999-05-005 or TCP::192.168.1.1
     myDeviceID = GetQpsModuleSelection(myQps)
-    #convert module to quarch module
-    myQuarchDevice = getQuarchDevice(myDeviceID, ConType = "QPS")
+
+    # If you know the name of the module you would like to talk to then you can skip module selection and hardcode the string.
+    # moduleStr = "USB:QTL1999-05-005"
+
+    # Convert module to Quarch module
+    print("\n\nConnecting to the selected device")
+    myQuarchDevice = getQuarchDevice(myDeviceID, ConType="QPS")
+
     # Create the device connection, as a QPS connected device
     myQpsDevice = quarchQPS(myQuarchDevice)
     myQpsDevice.openConnection()
 
     # Prints out connected module information
-    print("Running QPS Automation Example")
-    print("Module Name:")
-    print(myQpsDevice.sendCommand("hello?"))
+    print("\nConnected to module: " + myQpsDevice.sendCommand("hello?"))
 
     # Setup the voltage mode and enable the outputs
     setupPowerOutput(myQpsDevice)
 
-    # Set the averaging rate for the module.  This sets the resolution of data to record
-    # This is done via a direct command to the power module
+    # Set the averaging rate for the module. This sets the resolution of data to record.
+    # This is done via a direct command to the power module.
     print(myQpsDevice.sendCommand("record:averaging 32k"))
 
     # Start a stream, using the local folder of the script and a time-stamp file name in this example
     fileName = time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())    
     myStream = myQpsDevice.startStream(filePath + "\\" +  fileName)
-    print ("File output path set: " + filePath + "\\" + fileName)
+    print("File output path set: " + filePath + "\\" + fileName)
 
     '''
     Example of adding annotations to the trace.  This can be used to highlight events, errors or
     changes from one part of a test to another.
     
     Annotations can be added in real time, or placed anywhere on the trace as part of post-processing,
-    using a timestamp
+    using a timestamp.
     '''
     time.sleep (2)
     myStream.addAnnotation ('Adding an example annotation\\nIn real time!')
@@ -89,10 +114,10 @@ def main():
 
     '''
     Example of adding arbitrary data to the trace.  This allows IOPS, Temperature and similar to be added
-    where the data may be polled live from another process, or added in post-process
+    where the data may be polled live from another process, or added in post-process.
     
-    Channels need a name (T1 in this example) a 'group' name and a unit of measure
-    The final boolean will create auto SI unit ranges (milli/micro...) automatically if set to true
+    Channels need a name (T1 in this example) a 'group' name and a unit of measure.
+    The final boolean will create auto SI unit ranges (milli/micro...) automatically if set to true.
     '''
     myStream.addAnnotation('Starting temperature measurement here!')
     # Create new channel to record data into
@@ -107,11 +132,11 @@ def main():
 '''
 Simple function to check the output mode of the power module, setting it to 3v3 if required
 then enabling the outputs if not already done.  This will result in the module being turned on
-and supplying power
+and supplying power.
 '''
 def setupPowerOutput(myModule):
     # Output mode is set automatically on HD modules using an HD fixture, otherwise we will chose 5V mode for this example
-    outModeStr = myModule.sendCommand("config:output Mode?")
+    outModeStr = myModule.sendCommand("config:output mode?")
     if "DISABLED" in outModeStr:
         try:
             drive_voltage = raw_input(
@@ -131,9 +156,9 @@ def setupPowerOutput(myModule):
 
 
 '''
-Example function to write data to an arbitary channel that has been previously created
+Example function to write data to an arbitrary channel that has been previously created
 This data would normally come from another process such as drive monitor software or
-a traffic generator
+a traffic generator.
 '''
 def writeArbitaryData(myStream, channelName, groupName):
     print("Writings 10 seconds worth of (made up) temperature data, please wait...")
